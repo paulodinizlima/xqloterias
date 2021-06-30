16260d51fea9618d59b676533c0a26b0e3a11546
paulodinizlima <diniz_lima@hotmail.com>
2021-06-30 20:50:15 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>XQ Loterias</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
+	<!--Favicon -->
+	<link href="img/favicon.png" rel="icon">
+	<link href="img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+  	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  	<!-- Bootstrap CSS File -->
+  	<link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Main Stylesheet File -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+</head>
+
+
+<body>
+
+	<!--==========================
+    Header
+ 	============================-->
+
+ 	<header>
+    <h1 class="float-l">
+      <img src="img/logo.png">
+    </h1>
+
+    
+    <input type="checkbox" id="control-nav" />
+    <label for="control-nav" class="control-nav"></label>
+    <label for="control-nav" class="control-nav-close"></label>
+
+    <nav class="float-r">
+      <ul class="list-auto">
+        <li class="megasena">          
+          <a href="loterias/megasena/" title="Megasena"><span class="icone"><img src="img/icon_megasena.png" width="20"></span> Megasena</a>          
+        </li>
+        <li class="lotofacil">
+          <a href="loterias/lotofacil/" title="Lotofácil"><span class="icone"><img src="img/icon_lotofacil.png" width="20"></span> Lotofácil</a>
+        </li>
+        <li class="quina">
+          <a href="loterias/quina/" title="Quina"><span class="icone"><img src="img/icon_quina.png" width="20"></span> Quina</a>
+        </li>
+        <li class="lotomania">
+          <a href="loterias/lotomania/" title="Lotomania"><span class="icone"><img src="img/icon_lotomania.png" width="20"></span> Lotomania</a>
+        </li>
+        <li class="timemania">
+          <a href="loterias/timemania/" title="Timemania"><span class="icone"><img src="img/icon_timemania.png" width="20"></span> Timemania</a>
+        </li>
+        <li class="duplasena">
+          <a href="loterias/duplasena/" title="Dupla Sena"><span class="icone"><img src="img/icon_duplasena.png" width="20"></span> Dupla Sena</a>
+        </li>
+        <li class="diadesorte">
+          <a href="loterias/diadesorte/" title="Dia de Sorte"><span class="icone"><img src="img/icon_diadesorte.png" width="20"></span> Dia de Sorte</a>
+        </li>
+        <li class="supersete">
+          <a href="loterias/supersete/" title="Super Sete"><span class="icone"><img src="img/icon_supersete.png" width="20"></span> Super Sete</a>
+        </li>
+        <li class="federal">
+          <a href="loterias/federal/" title="Federal"><span class="icone"><img src="img/icon_federal.png" width="20"></span> Federal</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="containermain">
+    
+    <div class="main">
+      
+      <div class="left">
+          left
+
+      </div>
+      <div class="right">
+          right
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="containerfooter">
+    <div class="footer">
+      footer
+    </div>
+
+  </div>
+
+
+
+
+
+</body>
+</html>
---
+++ loterias/federal/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>XQ Loterias - Loteria Federal</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, maximum-scale=1, user-scalable=no" />
+
+	<!--Favicon -->
+	<link href="../../img/favicon.png" rel="icon">
+	<link href="../../img/apple-touch-icon.png" rel="apple-touch-icon">
+
+	<!-- Google Fonts -->
+  	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Montserrat:300,400,500,700" rel="stylesheet">
+
+  	<!-- Bootstrap CSS File -->
+  	<link href="../../lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- Main Stylesheet File -->
+	<link rel="stylesheet" type="text/css" href="../../css/style.css">
+	
+</head>
+
+
+<body>
+
+	<!--==========================
+    Header
+ 	============================-->
+
+ 	<header>
+    <h1 class="float-l">
+      <a href="../../"><img src="../../img/logo.png"></a>
+    </h1>
+
+    
+    <input type="checkbox" id="control-nav" />
+    <label for="control-nav" class="control-nav"></label>
+    <label for="control-nav" class="control-nav-close"></label>
+
+    <nav class="float-r">
+      <ul class="list-auto">
+        <li class="megasena">          
+          <a href="../megasena/" title="Megasena"><span class="icone"><img src="../../img/icon_megasena.png" width="20"></span> Megasena</a>          
+        </li>
+        <li class="lotofacil">
+          <a href="../lotofacil/" title="Lotofácil"><span class="icone"><img src="../../img/icon_lotofacil.png" width="20"></span> Lotofácil</a>
+        </li>
+        <li class="quina">
+          <a href="../quina/" title="Quina"><span class="icone"><img src="../../img/icon_quina.png" width="20"></span> Quina</a>
+        </li>
+        <li class="lotomania">
+          <a href="../lotomania/" title="Lotomania"><span class="icone"><img src="../../img/icon_lotomania.png" width="20"></span> Lotomania</a>
+        </li>
+        <li class="timemania">
+          <a href="../timemania/" title="Timemania"><span class="icone"><img src="../../img/icon_timemania.png" width="20"></span> Timemania</a>
+        </li>
+        <li class="duplasena">
+          <a href="../duplasena/" title="Dupla Sena"><span class="icone"><img src="../../img/icon_duplasena.png" width="20"></span> Dupla Sena</a>
+        </li>
+        <li class="diadesorte">
+          <a href="../diadesorte/" title="Dia de Sorte"><span class="icone"><img src="../../img/icon_diadesorte.png" width="20"></span> Dia de Sorte</a>
+        </li>
+        <li class="supersete">
+          <a href="../supersete/" title="Super Sete"><span class="icone"><img src="../../img/icon_supersete.png" width="20"></span> Super Sete</a>
+        </li>
+        <li class="federal">
+          <a href="" title="Federal"><span class="icone"><img src="../../img/icon_federal.png" width="20"></span> Federal</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="containermain">
+    
+    <div class="main">
+      <div class="tloterias tfederal">
+
+        <h6><strong>Loteria Federal - Resultado, Ganhadores e Prêmios</strong></h6>
+        <span class="font14">Confira hoje o resultado, ganhadores e prêmios da Mega Sena nos sorteios que são realizados na quarta-feira e sábado a partir das 20 horas online.</span>
+
+      </div>
+
+      <div class="left">
+          left
+
+      </div>
+      <div class="right">
+          
+          <div class="top_right">
+              top right
+
+          </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+  <div class="containerfooter">
+    <div class="footer">
+      footer
+    </div>
+
+  </div>
+
+
+
+
+
+</body>
+</html>
