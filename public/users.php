0c070792027dba6f63432ff0288d6abc507561f4
paulodinizlima <diniz_lima@hotmail.com>
2021-07-09 20:17:34 -0300

    Teste PDO

---
+++ public/users.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Sistema Crud - Lista de Usuários</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="crud">
+            <h3>Lista de Usuários</h3>
+            
+        </div>
+    </body>
+
+</html>
